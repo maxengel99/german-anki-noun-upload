f2943769bdfb1b76f52a67c6007c13a6818a442a
Max Engel <maxengel99@gmail.com>
2023-10-19 17:41:44 -0400

    Sort into found and not found words

--- main.py
+++ main.py
@@ -10,6 +10,9 @@
 
 import json
 import urllib.request
+from german_nouns.lookup import Nouns
+
+nouns_dictionary = Nouns()
 
 def is_noun(word):
     return word[0].isupper() and " " not in word
@@ -36,10 +39,27 @@ payloadTwo = {
 responseTwo = json.load(urllib.request.urlopen(
     'http://localhost:8765', json.dumps(payloadTwo).encode('utf-8')))
 
-noun_list = []
+noun_list = set()
 for card in responseTwo['result']:
     word = card['fields']['German']['value']
     if is_noun(word):
-        noun_list.append(word)
+        noun_list.add(word)
 
-print(noun_list)
+found_words = {}
+not_found_words = set()
+words_with_multiple_genders = set()
+
+for noun in noun_list:
+    noun_found = nouns_dictionary[noun]
+    if noun_found:
+        if 'genus' in noun_found[0]:
+            found_words[noun] = noun_found[0]['genus']
+        else:
+            words_with_multiple_genders.add(noun)
+        
+    else:
+        not_found_words.add(noun)
+
+print(found_words)
+print(not_found_words)
+print(words_with_multiple_genders)
