36f6b7688d34293c100dc7a384cf35f652925a94
Max Engel <maxengel99@gmail.com>
2023-10-20 19:42:30 -0400

    Upload to anki

--- main.py
+++ main.py
@@ -10,6 +10,7 @@
 
 import json
 import urllib.request
+import easygui
 from german_nouns.lookup import Nouns
 from anki_controller import AnkiController
 from word_processor import WordProcessor
@@ -20,8 +21,48 @@ word_processor = WordProcessor()
 
 current_card_ids = anki_controller.invoke("findCards", { "query": '"deck:German::German Class Words"'})
 current_cards_info = anki_controller.invoke("cardsInfo", { "cards": current_card_ids['result']})
-found_words, not_found_words, words_with_multiple_genders = word_processor.process_words(current_cards_info['result'])
+found_nouns, not_found_nouns, nouns_with_multiple_genders = word_processor.bucket_words(current_cards_info['result'])
 
-print(found_words)
-print(not_found_words)
-print(words_with_multiple_genders)
+current_gender_noun_card_ids = anki_controller.invoke("findCards", { "query": '"deck:German::German Gender Nouns"'})
+current_gender_noun_cards_info = anki_controller.invoke("cardsInfo", { "cards": current_gender_noun_card_ids['result']})
+words_to_upload_to_gender = word_processor.get_words_to_upload(found_nouns, current_gender_noun_cards_info)
+
+for word, gender in words_to_upload_to_gender.items():
+    fields = { "Noun": word, "Gender": gender }
+    deck_name = "German::German Gender Nouns"
+    model_name = "German Noun Gender"
+    json_args = {
+        "deckName": deck_name,
+        "modelName": model_name,
+        "fields": fields,
+        "options": { "allowDuplicate": False },
+        "tags": []
+    }
+    anki_controller.invoke("addNote", { "note": json_args })
+
+button_list = ["Masculine", "Feminine", "Neuter"]
+answer_to_field = {
+    "Masculine": 'M',
+    "Feminine": 'F',
+    "Neuter": 'N'
+}
+
+for word in not_found_nouns:
+    current_gender_nouns_set = set()
+    for noun in current_gender_noun_cards_info["result"]:
+        current_gender_nouns_set.add(noun["fields"]["Noun"]["value"])
+
+    if word not in current_gender_nouns_set:     
+        gender = easygui.buttonbox("The word '" + word + "' was not found. Please pick the gender of the this noun. https://www.collinsdictionary.com/us/dictionary/german-english/", "", button_list)
+        print(gender)
+        fields = { "Noun": word, "Gender": answer_to_field[gender] }
+        deck_name = "German::German Gender Nouns"
+        model_name = "German Noun Gender"
+        json_args = {
+            "deckName": deck_name,
+            "modelName": model_name,
+            "fields": fields,
+            "options": { "allowDuplicate": False },
+            "tags": []
+        }
+        anki_controller.invoke("addNote", { "note": json_args })
--- word_processor.py
+++ word_processor.py
@@ -8,26 +8,38 @@ class WordProcessor:
     def is_noun(self, word):
         return word[0].isupper() and " " not in word
 
-    def process_words(self, cards):
+    def bucket_words(self, cards):
         noun_list = set()
         for card in cards:
             word = card['fields']['German']['value']
             if self.is_noun(word):
                 noun_list.add(word)
 
-        found_words = {}
-        not_found_words = set()
-        words_with_multiple_genders = set()
+        found_nouns = {}
+        not_found_nouns = set()
+        nouns_with_multiple_genders = set()
 
         for noun in noun_list:
             noun_found = self.nouns_dictionary[noun]
             if noun_found:
                 if 'genus' in noun_found[0]:
-                    found_words[noun] = noun_found[0]['genus']
+                    found_nouns[noun] = noun_found[0]['genus']
                 else:
-                    words_with_multiple_genders.add(noun)
+                    nouns_with_multiple_genders.add(noun)
                 
             else:
-                not_found_words.add(noun)
+                not_found_nouns.add(noun)
         
-        return found_words, not_found_words, words_with_multiple_genders
+        return found_nouns, not_found_nouns, nouns_with_multiple_genders
+
+    def get_words_to_upload(self, found_nouns, current_gender_nouns):
+        current_gender_nouns_set = set()
+        words_to_upload_to_gender = {}
+        for noun in current_gender_nouns["result"]:
+            current_gender_nouns_set.add(noun["fields"]["Noun"]["value"])
+        
+        for word in found_nouns:
+            if word not in current_gender_nouns_set:
+                words_to_upload_to_gender[word] = self.nouns_dictionary[word][0]["genus"].upper()
+
+        return words_to_upload_to_gender
